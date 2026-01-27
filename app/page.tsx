348551bb16b08e131811e8e062a3245c0c05868b
Samuel Goldsmith <samuelrg2005@gmail.com>
2026-01-27 13:15:28 +1300

    CARD-26 Translations

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <div className="w-1/3">
           <TextBox text="The Kia Mouriora te Kaiwharawhara dashboard is designed to display ecological data about Te Kaiwharawhara. Navigate through information using the navigation bar and learn about the history, health, and conservation efforts of the region." />
           <ImageCycle pairs={imagePairs} header="Birds in the Catchment" header_url="https://www.visitzealandia.com/learn/nature-and-wildlife/"></ImageCycle>
-          <HoverTranslation text="Mouri"/>
         </div>
         <div className="w-2/3">
 
