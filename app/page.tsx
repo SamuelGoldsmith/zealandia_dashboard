2d6f59bff5cf7d6435fce3e041c96fc47a023491
Samuel Goldsmith <samuelrg2005@gmail.com>
2026-02-03 14:42:59 +1300

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const imagePairs = [
 ]
 export default function Home() {
   return (
-    <div className="min-w-screen flex min-h-screen bg-center bg-cover" style={{ backgroundImage: "url('/stream.avif')" }}>
+    <div className="min-w-screen flex min-h-screen bg-center bg-cover" style={{ backgroundImage: "url('/BACKGROUND.webp')" }}>
       <main className="min-h-screen w-full  flex">
         <div className="w-1/3">
           <TextBox text="The Kia Mouriora te Kaiwharawhara dashboard is designed to display ecological data about Te Kaiwharawhara. Navigate through information using the navigation bar and learn about the history, health, and conservation efforts of the region."/>
