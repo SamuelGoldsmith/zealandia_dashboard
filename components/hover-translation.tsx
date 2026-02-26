4cfc4cfac23437038a5fb18980c4a0f4daedabd5
o-wel <owenvhart@gmail.com>
2026-02-26 13:54:52 +1300

    pull translation data from google sheet

--- components/hover-translation.tsx
+++ components/hover-translation.tsx
@@ -5,12 +5,55 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 import { HoverCard, HoverCardTrigger } from "@radix-ui/react-hover-card"
 import { HoverCardContent } from "./ui/hover-card"
-import translations from "@/lib/translations.json"
+import {useEffect} from "react";
+import {useState} from "react";
+import Papa, {ParseResult} from "papaparse";
+
+const translationLink: string = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQx-MJXFVWXP1KqLkkxECQK7Gwiqn9nk_84gM0-6t1MbBH_HolAsf9o223PhgsU77GbRl9twltB9r8M/pub?gid=1728029904&single=true&output=csv"
+
+interface TranslationRow {
+    word: string;
+    translation: string;
+}
+
 function HoverTranslation({
     text,
     className,
   ...props
 }:{ text: string, className?: string } & React.HTMLAttributes<HTMLSpanElement>) {
+
+    const [translations, setTranslations] = useState<Record<string, string>>({})
+
+    useEffect(() => {
+
+        async function loadCSV() {
+
+            const t: Record<string, string> = {}
+
+            if (!translationLink) {
+                console.error("CSV URL is not defined in environment variables.");
+                return;
+            }
+            const res = await fetch(translationLink)
+            if (!res.ok) throw new Error(`Failed to fetch CSV: ${res.statusText}`);
+            const csv: string = await res.text();
+
+            const results: ParseResult<TranslationRow> = Papa.parse<TranslationRow>(csv, {
+                header: true,
+                skipEmptyLines: true,
+            });
+
+            results.data.forEach((item) => {
+                t[item.word] = item.translation;
+            })
+
+            setTranslations(t)
+        }
+
+        loadCSV();
+    }, []);
+
+
   return (
     <span className={cn(`cursor-pointer`, className)} {...props}>
       <HoverCard>
